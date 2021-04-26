db412261f2850efac4c3079c16bbe11dd8d079f5
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-26 12:08:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+function saturdayFun(word = "roller-skate"){
+  // console.log(`This Saturday, I want to ${word}!`)
+  return `This Saturday, I want to ${word}!`
+}
+// saturdayFun("eat")
+
+let mondayWork = function(word = 'go to the office'){
+  // console.log(`Monday, I will ${word}.`)
+  return `This Monday, I will ${word}.`
+}
+// mondayWork("eat")
